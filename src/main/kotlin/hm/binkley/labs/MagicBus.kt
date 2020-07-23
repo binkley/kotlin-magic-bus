d03f8f65448a6e588ad7c4fbebae5ec4a1adc498
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-07-23 06:17:20 -0500

    Docs

--- src/main/kotlin/hm/binkley/labs/MagicBus.kt
+++ src/main/kotlin/hm/binkley/labs/MagicBus.kt
@@ -3,11 +3,29 @@ package hm.binkley.labs
 typealias Mailbox<T> = (T) -> Unit
 
 interface MagicBus {
+    /** Posts messages of [messageType] to [mailbox]. */
     fun <T> subscribe(messageType: Class<T>, mailbox: Mailbox<in T>)
+    /** Stops posting messages of [messageType] to [mailbox]. */
     fun <T> unsubscribe(messageType: Class<T>, mailbox: Mailbox<in T>)
+    /** Posts [message] to any subscribed mailboxes. */
     fun post(message: Any)
 }
 
+/** Subscribes without caller providing a class object. */
+inline fun <reified T> Mailbox<in T>.deliverTo(bus: MagicBus) =
+    bus.subscribe(T::class.java, this)
+
+/** Unsubscribes without caller providing a class object. */
+inline fun <reified T> Mailbox<in T>.noDeliveryTo(bus: MagicBus) =
+    bus.unsubscribe(T::class.java, this)
+
+/** Subscribe to [ReturnedMessage] to find out about posts to no mailbox. */
+data class ReturnedMessage(
+    val bus: MagicBus,
+    val message: Any
+)
+
+/** Subscribe to [FailedMessage] to find out about broken mailboxes. */
 data class FailedMessage(
     val bus: MagicBus,
     val mailbox: Mailbox<*>,
@@ -15,22 +33,14 @@ data class FailedMessage(
     val failure: Exception
 )
 
-data class ReturnedMessage(
-    val bus: MagicBus,
-    val message: Any
-)
-
-data class DiscardMailbox<T>(val type: Class<T>) : Mailbox<T> {
+/** Creates a mailbox which throws away messages of [messageType]. */
+data class DiscardMailbox<T>(val messageType: Class<T>) : Mailbox<T> {
     override operator fun invoke(message: T) = Unit
 }
 
+/** Creates a [DiscardMailbox] without providing a class object. */
 inline fun <reified T> discard(): Mailbox<T> = DiscardMailbox(T::class.java)
 
+/** Creates a mailbox which always fails. */
 fun <T, E : Exception> failWith(exceptionCtor: () -> E): Mailbox<T> =
     { throw exceptionCtor() }
-
-inline fun <reified T> Mailbox<in T>.deliverTo(bus: MagicBus) =
-    bus.subscribe(T::class.java, this)
-
-inline fun <reified T> Mailbox<in T>.noDeliveryTo(bus: MagicBus) =
-    bus.unsubscribe(T::class.java, this)
