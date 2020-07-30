7079293cf0da9add384f11c8ded14685b250e0c3
Brian Oxley <boxley@thoughtworks.com>
2020-07-30 10:33:14 -0500

    Docs

--- src/main/kotlin/hm/binkley/labs/MagicBus.kt
+++ src/main/kotlin/hm/binkley/labs/MagicBus.kt
@@ -29,14 +29,31 @@ inline fun <reified T> MagicBus.subscribe(noinline mailbox: Mailbox<in T>) =
 inline fun <reified T> MagicBus.unsubscribe(noinline mailbox: Mailbox<in T>) =
     unsubscribe(T::class.java, mailbox)
 
-/** Creates a mailbox which throws away messages of [messageType]. */
+/**
+ * Creates a mailbox which throws away messages of [messageType].  Use case:
+ * To avoid [ReturnedMessage] posts for message types not of interest, use a
+ * discard mailbox.
+ *
+ * *NB* &mdash; An extreme example is:
+ * ```
+ * bus.subscribe(discard<ReturnedMessage>())
+ * ```
+ * which ignores all messages that have no mailboxes.  However, this is an
+ * _anti-pattern_, as it covers up bugs in typing or messaging designs.
+ *
+ * @see discard
+ */
 @Suppress("UnusedPrivateMember")
 class DiscardMailbox<T>(private val messageType: Class<T>) : Mailbox<T> {
     override operator fun invoke(message: T) = Unit
     override fun toString() = "DISCARD-MAILBOX"
 }
 
-/** Creates a [DiscardMailbox] without providing a class object. */
+/**
+ * Creates a discard mailbox without providing a class object.
+ *
+ * @see DiscardMailbox
+ */
 inline fun <reified T> discard(): Mailbox<T> = DiscardMailbox(T::class.java)
 
 /** Creates a mailbox which always fails. */
