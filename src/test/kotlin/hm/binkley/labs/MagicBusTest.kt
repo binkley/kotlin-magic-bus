910d3f2a8d8c77c8c658f914f5fb783c5fa1e7d5
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-09-28 06:12:22 -0500

    Explanatory comment

--- src/test/kotlin/hm/binkley/labs/MagicBusTest.kt
+++ src/test/kotlin/hm/binkley/labs/MagicBusTest.kt
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test
 internal class MagicBusTest {
     private val bus = DEFAULT_BUS
 
+    /**
+     * This test looks pointless.  It satisfies JaCoCo that lazy init worked,
+     * essentially testing that the Kotlin library works.  :(
+     */
     @Test
     fun `should share global default bus with same instance`() {
         assertThat(bus).isSameAs(DEFAULT_BUS)
